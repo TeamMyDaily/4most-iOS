3dc8bcbfb050539b3ddf4920b3f903e6eebe9ff2
lee-yujinn <leeyj7988@swu.ac.kr>
2020-12-29 17:05:48 +0900

    [Feat] 초기설정

---
+++ Mydaily_iOS/Mydaily_iOS/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mydaily_iOS
+//
+//  Created by 이유진 on 2020/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
