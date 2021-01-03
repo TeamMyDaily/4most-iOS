f232439f99339992be16f291ed5021d8b714093b
lee-yujinn <leeyj7988@naver.com>
2021-01-04 01:19:47 +0900

    [Feat] 이메일 유효성 검사

--- Mydaily_iOS/Mydaily_iOS/Source/LoginScene/View/VC/registerVC.swift
+++ Mydaily_iOS/Mydaily_iOS/Source/LoginScene/View/VC/registerVC.swift
@@ -14,10 +14,15 @@ class RegisterVC: UIViewController {
     @IBOutlet weak var pwTextField: UITextField!
     @IBOutlet weak var checkpwTextField: UITextField!
     
+    @IBOutlet weak var validateEmailLabel: UILabel!
+    @IBOutlet weak var validatePWLabel: UILabel!
+    @IBOutlet weak var checkPWLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationBar()
+        changeTextFields()
     }
     
 }
@@ -49,32 +54,63 @@ extension RegisterVC {
 }
 
 // MARK: - TextField,Button
-//extension RegisterVC {
-//    private func changeTextFields() {
-//        nameTextField.addTarget(self, action: #selector(changenameTextFieldUI), for: .allEditingEvents)
-//        pwTextField.addTarget(self, action: #selector(changepwTextfiledUI), for: .allEditingEvents)
-//    }
-//    
-//    @objc
-//    func changenameTextFieldUI(){
-//        nameTextField.backgroundColor = .white
-//        nameTextField.layer.borderColor = UIColor.red.cgColor
-//        nameTextField.layer.borderWidth = 1.5
-//        
-//        
-//    }
-//    
-//    @objc
-//    func changepwTextfiledUI(){
-//        pwTextField.backgroundColor = .white
-//        pwTextField.layer.borderColor = UIColor.red.cgColor
-//        pwTextField.layer.borderWidth = 1.5
-//        
-//        if nameTextField.text?.isEmpty
+extension RegisterVC {
+    private func changeTextFields() {
+        nameTextField.addTarget(self, action: #selector(changenameTextFieldUI), for: .allEditingEvents)
+        emailTextField.addTarget(self, action: #selector(changeemailTextfiledUI), for: .allEditingEvents)
+    }
+    
+    @objc
+    func changenameTextFieldUI(){
+        changeTextfieldUI(textfield: nameTextField)
+    }
+    
+    @objc
+    func changeemailTextfiledUI(){
+        changeTextfieldUI(textfield: emailTextField)
+        
+        if !(emailTextField.text!.validateEmail()) {
+            validateEmailLabel.text = "사용 불가능한 이메일이에요!"
+        }
+        else{
+            validateEmailLabel.text = ""
+        }
+    }
+    
+    func changeTextfieldUI(textfield: UITextField){
+        if !(textfield.text!.isEmpty){
+            textfield.backgroundColor = .white
+            textfield.layer.borderColor = UIColor.red.cgColor
+            textfield.layer.borderWidth = 1.5
+        }
+        else{
+            textfield.layer.borderColor = UIColor.gray.cgColor
+        }
+        
+//        if !(idTextField.text!.isEmpty) && !(pwTextField.text!.isEmpty){
 //            loginButton.isEnabled = true
 //        }
 //        else{
 //            loginButton.isEnabled = false
 //        }
-//    }
-//}
+    }
+}
+
+//MARK: - String
+extension String {
+    // E-mail address validation
+    public func validateEmail() -> Bool {
+        let emailRegEx = "^.+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*$"
+        
+        let predicate = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return predicate.evaluate(with: self)
+    }
+    
+    // Password validation
+    public func validatePassword() -> Bool {
+        let passwordRegEx = "^(?=.*[0-9])(?=.*[a-z]).{6}$"
+        
+        let predicate = NSPredicate(format:"SELF MATCHES %@", passwordRegEx)
+        return predicate.evaluate(with: self)
+    }
+}
