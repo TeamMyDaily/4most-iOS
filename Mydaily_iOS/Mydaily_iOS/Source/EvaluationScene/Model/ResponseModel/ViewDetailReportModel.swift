339841cb43132f9c8306659ca55368800e6f4780
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-01-15 16:10:44 +0900

    [Fix] 회고뷰 서버 연결 수정

--- Mydaily_iOS/Mydaily_iOS/Source/EvaluationScene/Model/ResponseModel/ViewDetailReportModel.swift
+++ Mydaily_iOS/Mydaily_iOS/Source/EvaluationScene/Model/ResponseModel/ViewDetailReportModel.swift
@@ -17,21 +17,23 @@ struct ViewDetailReportModel: Codable {
 
 // MARK: - ViewDetailRetrospectiveResponse
 struct ViewDetailReportResponse: Codable {
+    let totalKeywordId: Int
     let keywordName: String
     let goal: String?
     let isGoalCompleted: Bool?
+    let weekGoalId: Int?
     let goalExist: Bool
     let tasks: [Tasks]
 }
 
 // MARK: - Task
 struct Tasks: Codable {
-    let taskID: Int
+    let taskId: Int
     let title, date: String
     let satisfaction: Int
 
     enum CodingKeys: String, CodingKey {
-        case taskID = "taskId"
+        case taskId = "taskId"
         case title, date, satisfaction
     }
 }
